94311bfe5622cdccbbfa732de6cc9cb40f4a43de
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-12 23:28:28 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,27 @@
+import string
+import random
+
+
+class Database:
+    def __init__(self) -> None:
+        self.database = {}
+        print("Database init")
+    
+    def add(self, data):
+        key = self.__generate_unique_key()
+        self.database[key] = data
+        return key
+
+    def get(self, key):
+        return self.database[key]
+
+    def __generate_unique_key(self):
+        letters = string.ascii_letters + string.digits
+        key = ""
+        for i in range(16):
+            key += letters[random.randint(0, len(letters))]
+        if key in self.database.keys():
+            return self.__generate_unique_key() 
+        return key
+
+
