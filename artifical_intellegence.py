d274bb21d311561501d44d68b606dfd9ed7a7a83
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-12 23:28:30 +0300

    Update artifical_intellegence.py

--- artifical_intellegence.py
+++ artifical_intellegence.py
@@ -1,49 +1,45 @@
 import os
-os.add_dll_directory("C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v10.1")
 from tensorflow.keras.datasets import cifar10 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization
 from tensorflow.keras.optimizers import Adam, Adadelta 
 from tensorflow.keras import utils
 from tensorflow.keras.preprocessing import image
-from tensorflow.python.client import device_lib 
-print(device_lib.list_local_devices())
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import random
-import math
 
 
 class AI:
   def __init__(self, database_path, batch_size):
     self.__database_path = database_path
     self.__batch_size = batch_size
-    self.__generate_labels()
-    self.__load_image_list()
-    self.__init_trains()
-    self.__load_images()
-    self.__xy_logging()
-    self.__compile_model()
-    self.__log_model()
-    self.__studying_ai()
-    self.__studying_log()
+    self.generate_labels()
+    self.load_image_list()
+    self.init_trains()
+    self.load_images()
+    self.xy_logging()
+    self.compile_model()
+    # self.log_model()
+    self.studying_ai()
+    # self.studying_log(10)
   
-  def __generate_labels(self):
+  def generate_labels(self):
     future_labels = []
-    self.__labels = {}
+    self.labels = {}
     for maybedir in os.listdir(path=self.__database_path):
       future_labels.append(maybedir)
     for key in future_labels:
-      self.__labels[key] = [0 for _ in range(len(future_labels))]
-    for i in range(len(self.__labels.keys())):  
+      self.labels[key] = [0 for _ in range(len(future_labels))]
+    for i in range(len(self.labels.keys())):  
       key = future_labels[i]
-      self.__labels[key][i] = 1
+      self.labels[key][i] = 1
     del future_labels
 
-  def __load_image_list(self):
+  def load_image_list(self):
     self.__image_list = []
-    for label in self.__labels.keys():
+    for label in self.labels.keys():
       dir = self.__database_path + "/" + label
       flist = os.listdir(dir)
       for f in flist:
@@ -51,14 +47,14 @@ class AI:
         self.__image_list.append(fpath)
     random.shuffle(self.__image_list)
 
-  def __init_trains(self):
-    self.__x_train = []
-    self.__y_train = []
+  def init_trains(self):
+    self.x_train = []
+    self.y_train = []
   
-  def __smart_trimming(self, img):
+  def smart_trimming(self, img):
     img_w, img_h = img.size
-    target_size = [120, 120]
-    if self.__format_is_album(img_w, img_h):
+    target_size = [50, 50]
+    if self.format_is_album(img_w, img_h):
       new_h = target_size[1]
       new_w = round(new_h / img_h * img_w)
     else:
@@ -71,78 +67,78 @@ class AI:
     img = img.crop((top_left[0], top_left[1], bottom_right[0], bottom_right[1]))
     return img
 
-  def __format_is_album(self, width: int, height: int):
+  def format_is_album(self, width: int, height: int):
     return True if width > height else False
 
-  def __load_images(self):
+  def load_images(self):
     for fpath in self.__image_list:
       img = Image.open(fpath)
-      img = self.__smart_trimming(img)
+      img = self.smart_trimming(img)
       img = np.array(img)
-      self.__x_train.append(img)
+      self.x_train.append(img)
       label = fpath.split('/')[-1].split('.')[0]
-      self.__y_train.append(self.__labels[label])
+      self.y_train.append(self.labels[label])
 
-    self.__x_train = np.array(self.__x_train)
-    self.__y_train = np.array(self.__y_train)
+    self.x_train = np.array(self.x_train)
+    self.y_train = np.array(self.y_train)
 
-    self.__x_val = self.__x_train[0:7000]
-    self.__y_val = self.__y_train[0:7000]
+    self.x_val = self.x_train[0:7000]
+    self.y_val = self.y_train[0:7000]
 
-    self.__x_test = self.__x_train[7000:10000]
-    self.__y_test = self.__y_train[7000:10000]
+    self.x_test = self.x_train[7000:10000]
+    self.y_test = self.y_train[7000:10000]
 
-    self.__x_train = self.__x_train[10000:]
-    self.__y_train = self.__y_train[10000:]
+    self.x_train = self.x_train[10000:]
+    self.y_train = self.y_train[10000:]
   
 
-  def __xy_logging(self):
+  def xy_logging(self):
     print("Обучающая выборка:")
-    print(self.__x_train.shape)
-    print(self.__y_train.shape)
+    print(self.x_train.shape)
+    print(self.y_train.shape)
 
     print("Проверочная выборка:")
-    print(self.__x_val.shape)
-    print(self.__y_val.shape)
+    print(self.x_val.shape)
+    print(self.y_val.shape)
 
     print("Тестовая выборка:")
-    print(self.__x_test.shape)
-    print(self.__y_test.shape)
+    print(self.x_test.shape)
+    print(self.y_test.shape)
 
-  def __compile_model(self):
-    self.__model = Sequential()
-    self.__model.add(BatchNormalization(input_shape=(120, 120, 3), name="bn1"))
-    self.__model.add(Conv2D(32, (3, 3), padding='same', activation='relu', name="Conv2D-layer1"))
-    self.__model.add(Conv2D(32, (3, 3), padding='same', activation='relu', name="Conv2D-layer2"))
-    self.__model.add(MaxPooling2D(pool_size=(2, 2), name="mp2D-layer1"))
-    self.__model.add(Dropout(0.25, name="Dropout-layer1"))
-    self.__model.add(BatchNormalization(name = "bn2"))
-    self.__model.add(Conv2D(64, (3, 3), padding='same', activation='relu', name = "Conv2D-layer3"))
-    self.__model.add(Conv2D(64, (3, 3), padding='same', activation='relu', name = "Conv2D-layer4"))
-    self.__model.add(MaxPooling2D(pool_size=(2, 2), name="mp2D-layer2"))
-    self.__model.add(Dropout(0.25, name="Dropout-layer2"))
-    self.__model.add(BatchNormalization(name = "bn3"))
-    self.__model.add(Conv2D(128, (3, 3), padding='same', activation='relu', name = "Conv2D-layer5"))
-    self.__model.add(Conv2D(128, (3, 3), padding='same', activation='relu', name = "Conv2D-layer6"))
-    self.__model.add(MaxPooling2D(pool_size=(2, 2), name="mp2D-layer3"))
-    self.__model.add(Dropout(0.25, name="Dropout-layer3"))
-    self.__model.add(Flatten(name="flatten"))
-    self.__model.add(Dense(len(self.__labels.keys()), activation='softmax', name = "labels"))
-    #model.compile(loss="categorical_crossentropy", optimizer=Adam(learning_rate=0.01), metrics=["accuracy"])
-    self.__model.compile(loss="categorical_crossentropy", optimizer=Adam(), metrics=["accuracy"])
+  def compile_model(self):
+    self.model = Sequential()
+    self.model.add(BatchNormalization(input_shape=(50, 50, 3), name="bn1"))
+    self.model.add(Conv2D(32, (3, 3), padding='same', activation='relu', name="Conv2D-layer1"))
+    self.model.add(Conv2D(32, (3, 3), padding='same', activation='relu', name="Conv2D-layer2"))
+    self.model.add(MaxPooling2D(pool_size=(2, 2), name="mp2D-layer1"))
+    self.model.add(Dropout(0.25, name="Dropout-layer1"))
+    self.model.add(BatchNormalization(name = "bn2"))
+    self.model.add(Conv2D(64, (3, 3), padding='same', activation='relu', name = "Conv2D-layer3"))
+    self.model.add(Conv2D(64, (3, 3), padding='same', activation='relu', name = "Conv2D-layer4"))
+    self.model.add(MaxPooling2D(pool_size=(2, 2), name="mp2D-layer2"))
+    self.model.add(Dropout(0.25, name="Dropout-layer2"))
+    self.model.add(BatchNormalization(name = "bn3"))
+    self.model.add(Conv2D(128, (3, 3), padding='same', activation='relu', name = "Conv2D-layer5"))
+    self.model.add(Conv2D(128, (3, 3), padding='same', activation='relu', name = "Conv2D-layer6"))
+    self.model.add(MaxPooling2D(pool_size=(2, 2), name="mp2D-layer3"))
+    self.model.add(Dropout(0.25, name="Dropout-layer3"))
+    self.model.add(Flatten(name="flatten"))
+    self.model.add(Dense(len(self.labels.keys()), activation='softmax', name = "labels"))
+    self.model.compile(loss="categorical_crossentropy", optimizer=Adam(learning_rate=0.01), metrics=["accuracy"])
+    # self.__model.compile(loss="categorical_crossentropy", optimizer=Adam(), metrics=["accuracy"])
 
-  def __log_model(self):
-    self.__model.summary()
+  def log_model(self):
+    self.model.summary()
 
-  def __studying_ai(self):
-    self.__history = self.__model.fit(self.__x_train, 
-                            self.__y_train, 
+  def studying_ai(self):
+    self.__history = self.model.fit(self.x_train, 
+                            self.y_train, 
                             batch_size=self.__batch_size, 
                             epochs=20,
-                            validation_data=(self.__x_val,self.__y_val),
+                            validation_data=(self.x_val,self.y_val),
                             verbose=1)
 
-  def __studying_log(self):
+  def studying_log(self, n: int):
     plt.plot(self.__history.history['accuracy'], 
          label='Доля верных ответов на обучающем наборе')
     plt.plot(self.__history.history['val_accuracy'], 
@@ -152,15 +148,7 @@ class AI:
     plt.legend()
     plt.show()
 
-    prediction = self.__model.predict(self.__x_test)
-    n = 123
-
-    img = self.__x_test[n]
-    img = img.reshape(120, 120, 3)
-    img = img.astype('uint8')
-    plt.figure(figsize=(2, 2))
-    plt.imshow(Image.fromarray(img))
-    plt.show()
+    prediction = self.model.predict(self.x_test)
 
     print("Выход сети:")
     print(prediction[n])
@@ -168,11 +156,7 @@ class AI:
     for i in range(2):
       print(i,"->","{:.40f}".format(prediction[n][i]))
     a = np.argmax(prediction[n])
-    if a == 0:
-      b = 'dog'
-    else:
-      b = 'cat'
-    print("Распознан объект: ", a, "-", b)
-    print("Верный ответ: ", a, "-", b)
+      
+    print("Распознан объект: ", a, "-", list(self.labels.keys())[a])
+    print("Верный ответ: ", a, "-", list(self.labels.keys())[a])
   
-sa = AI(database_path="img", batch_size=128)
