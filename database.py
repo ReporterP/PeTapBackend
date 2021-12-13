29c29a768e5ebfd7ba460f2c9309726bdd0b897e
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2021-12-13 11:40:03 +0300

    Update database.py

--- database.py
+++ database.py
@@ -4,21 +4,27 @@ import random
 
 class Database:
     def __init__(self) -> None:
-        self.database = {}
+        self._database = {}
     
     def add(self, data):
         key = self.__generate_unique_key()
-        self.database[key] = data
+        self._database[key] = data
         return key
 
     def get(self, key):
-        return self.database[key]
+        return self._database[key]
+
+    def keys(self):
+        return self._database.keys()
+
+    def values(self):
+        return self._database.values()
 
     def __generate_unique_key(self):
         letters = string.ascii_letters + string.digits
         key = ""
         for i in range(16):
             key += letters[random.randint(0, len(letters))]
-        if key in self.database.keys():
+        if key in self._database.keys():
             return self.__generate_unique_key() 
         return key
